e074d22bc1ab949359b4bc425ccf3852302b5492
Umama11-arch <umamaishaq11@gmail.com>
2026-09-17 07:04:21 +0500

    CORS Fixed

--- backend/TODOAPP.js
+++ backend/TODOAPP.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 const cors = require("cors");
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://prody-gamma.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
